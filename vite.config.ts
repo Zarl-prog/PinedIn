4d84402e382d7c1a163ae880661ebba422778204
Zarl-prog <asimjunaidi5u@gmail.com>
2026-06-02 02:35:18 +0530

    chore(build): add task-card.html entry, remove overlay entry

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(async () => ({
     rollupOptions: {
       input: {
         main: "index.html",
-        overlay: "overlay.html"
+        "task-card": "task-card.html",
       }
     }
   },

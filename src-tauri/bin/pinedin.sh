68f36fdedd19f94b625d77d0ffb000f3f984b205
Asim Junaidi <asimworks.dev@gmail.com>
2026-06-28 02:50:40 +0530

    fix webkit crash on appimage: all wrappers set GDK_BACKEND=x11 + custom .desktop injected into appimage

--- src-tauri/bin/pinedin.sh
+++ src-tauri/bin/pinedin.sh
@@ -3,4 +3,5 @@
 # Sets WebKitGTK environment variables to prevent GPU compositing crashes.
 export WEBKIT_DISABLE_COMPOSITING_MODE=1
 export WEBKIT_DISABLE_DMABUF_RENDERER=1
+export GDK_BACKEND=x11
 exec "$(dirname "$(readlink -f "$0")")/pinedin" "$@"

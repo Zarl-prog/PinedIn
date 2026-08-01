ce60851055afda65d72c2fb5225976524acb6055
Asim Junaidi <asimworks.dev@gmail.com>
2026-08-02 00:46:08 +0530

    chore: bump AGP to 8.13.2 and Gradle wrapper to 8.12.3

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file. Plugin versions are declared here and applied per-module.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
     // Kotlin 2.0+ ships the Compose compiler as a Gradle plugin rather than
     // a composeOptions extension version.
